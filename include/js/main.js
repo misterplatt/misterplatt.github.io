5c1f6ca64aaa80315608450b2842be8e72b0d954
Hayden Platt <misterplatt17@gmail.com>
2015-12-20 11:23:50 -0800

    Adding DL links to videos

--- include/js/main.js
+++ include/js/main.js
@@ -188,6 +188,10 @@ BRUSHED.fancyBox = function(){
 			beforeShow: function () {
 					this.title = $(this.element).attr('title');
 					this.title = '<h4>' + this.title + '</h4>' + '<p>' + $(this.element).parent().find('img').attr('alt') + '</p>';
+					// Add download button
+					if($(this.element).parent().find('img').attr('dlink') != null){
+						this.title += '<br>' + '<a href="' + $(this.element).parent().find('img').attr('dlink') + '" class="button""' + $(this.element).parent().find('img').attr('dlink') + '">Download</a>';
+					}
 			},
 			helpers : {
 				title : { type: 'inside' },
